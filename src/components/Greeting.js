7e19671413c020fbefc936e7629d269a5457ba23
wayungi <fwayungi@gmail.com>
2022-12-17 16:30:36 +0300

    ADDS: Display API data to browser

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,26 @@
-import { useSelector } from 'react-redux';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreeting } from '../redux/greetingSlice';
 
 const Greeting = () => {
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
   const { message } = useSelector((state) => state.greet);
+
+  useEffect(() => {
+    if (loading) { return; }
+    setLoading(true);
+    dispatch(fetchGreeting()).then(() => {
+      setLoading(false);
+    }).catch(() => {
+      setLoading(false);
+    });
+  }, []);
 
   return (
     <div>
+      <h1>TO GREET 2</h1>
       { message }
     </div>
   );
--- src/redux/greetingSlice.js
+++ src/redux/greetingSlice.js
@@ -1,18 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   message: '',
   loading: false,
-  error: null,
+  error: '',
 };
+
+const fetchGreeting = createAsyncThunk('greeting/fetchGreeting', () => axios.get('http://127.0.0.1:3000/messages', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}).then((response) => response.data.message));
 
 export const greetingSlice = createSlice({
   name: 'greets',
   initialState,
   reducers: {},
-  extraReducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchGreeting.pending, (state) => {
+      // eslint-disable-next-line no-param-reassign
+      state.loading = true;
+    });
+
+    builder.addCase(fetchGreeting.fulfilled, (state, action) => {
+      // eslint-disable-next-line no-unused-expressions, no-param-reassign
+      state.loading = false;
+      // eslint-disable-next-line no-unused-expressions, no-param-reassign
+      state.message = action.payload;
+      // eslint-disable-next-line no-unused-expressions, no-param-reassign
+      state.error = '';
+    });
+
+    builder.addCase(fetchGreeting.rejected, (state, action) => {
+      // eslint-disable-next-line no-unused-expressions, no-param-reassign
+      state.loading = false;
+      // eslint-disable-next-line no-unused-expressions, no-param-reassign
+      state.message = '';
+      // eslint-disable-next-line no-unused-expressions, no-param-reassign
+      state.error = action.error.message;
+    });
+  },
 });
 
-// Action creators are generated for each case reducer function
-export const { x } = greetingSlice.actions;
+export { fetchGreeting };
 export default greetingSlice.reducer;
