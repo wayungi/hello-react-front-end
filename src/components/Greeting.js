804a9328cb86479f6583f54376bd3070edfd172f
wayungi <fwayungi@gmail.com>
2022-12-17 18:18:19 +0300

    REMOVES: linter disable rule

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchGreeting } from '../redux/greetingSlice';
